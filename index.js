a9894f61f19d421e947bd56ea356f86bb14cd70a
Pau García Chiner <pauchiner@Paus-MacBook-Air.local>
2025-05-14 13:52:45 +0200

    fix: remove the index.html since is not needed anymore

--- index.js
+++ index.js
@@ -6,4 +6,23 @@ const routes = {
 	"/": { view: () => m(Home) },
 };
 
-export default m.route(null, "/", routes);
+// Here you can define your properties of the document.
+const context = {
+  document: {
+    lang: "es",
+    title: "sin.js",
+    head: [
+      m("meta", {charset: "UTF-8"})
+    ]
+  }
+}
+
+m.css`
+  html, body {
+    margin: 0;
+    width: 100%;
+    height: 100%;
+  }
+`
+
+export default m.route(context, "/", routes);
